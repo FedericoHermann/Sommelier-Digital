1b4d5f75eb5954d500451ed5ef66fba59304e473
Federico Andres Hermann <federico.hermann@apmterminals.com>
2026-05-05 20:58:39 -0300

    Add radar charts + visual styling improvements

--- src/analytics.py
+++ src/analytics.py
@@ -23,7 +23,7 @@ def plot_radar_chart(row, user_vector=None):
     angles += [angles[0]]
 
     # para cambiar tamaño
-    fig, ax = plt.subplots(figsize=(2, 2), subplot_kw=dict(polar=True))
+    fig, ax = plt.subplots(figsize=(1, 1), subplot_kw=dict(polar=True))
 
     # para cambiar estilos de color 
     accent_color = "#E0430F"   
@@ -34,24 +34,24 @@ def plot_radar_chart(row, user_vector=None):
     ax.set_facecolor("#0E1117")
 
     # vino
-    ax.plot(angles, wine_values, linewidth=0.5, color=accent_color)
+    ax.plot(angles, wine_values, linewidth=0.3, color=accent_color)
     ax.fill(angles, wine_values, color=accent_color, alpha=0.25)
 
     # usuario (línea punteada)
     if user_vector is not None:
         user_values = user_vector[:6].tolist()
         user_values += [user_values[0]]
-        ax.plot(angles, user_values, linestyle="dashed", linewidth=0.5, color=user_color)
+        ax.plot(angles, user_values, linestyle="dashed", linewidth=0.3, color=user_color)
 
     # etiquetas
     ax.set_xticks(angles[:-1])
-    ax.set_xticklabels(labels, color="white", fontsize=5)
+    ax.set_xticklabels(labels, color="white", fontsize=2)
 
     # escala radial
     ax.set_yticks([1, 2, 3, 4, 5])
-    ax.set_yticklabels([], color="white", fontsize=5)
+    ax.set_yticklabels([], color="white", fontsize=2)
 
     # grilla en gris suave
-    ax.grid(color="gray", linestyle="dotted", linewidth=0.5)
+    ax.grid(color="gray", linestyle="dotted", linewidth=0.3)
 
     return fig
